db31a931f0e659dfd654953d1863b70b7d684cec
paulmwale15-mil <paulmwale15@gmail.com>
2025-08-22 11:57:07 +0200

    Create Navbar.js

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "1rem", textAlign: "center", fontWeight: "bold" }}>
+      <h2>TIPEZANE</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
