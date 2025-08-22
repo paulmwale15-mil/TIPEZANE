2f9a66de6171c83ed8b215c6fa778fa43f05b08e
paulmwale15-mil <paulmwale15@gmail.com>
2025-08-22 12:01:01 +0200

    Create Footer.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "1rem", background: "#111", color: "#fff" }}>
+      <p>© 2023 TIPEZANE. All rights reserved.</p>
+      <p>About Us | Contact Us | Terms & Conditions | Privacy Policy</p>
+    </footer>
+  );
+}
+
+export default Footer;
